27fe9ea51f2ba4ed4bff3711646836e279763817
lonely-chicken <lonely.and.sad.chicken@gmail.com>
2025-03-10 15:58:08 -0700

    .

--- orginal/select.js
+++ orginal/select.js
@@ -1,30 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const inputWrap = document.getElementById("inputWrap");
-  const option = document.getElementById("option");
-  const options = document.querySelectorAll(".option p");
+function show(value) {
+  document.querySelector(".text-box").value = value;
+}
 
-  inputWrap.addEventListener("click", showOption);
-  options.forEach( item => {
-    item.addEventListener('click', showVal);
-  })
-
-  function showOption() {
-    inputWrap.classList.toggle("border-bottom-n");
-    inputWrap.classList.toggle("active");
-    option.classList.toggle("d-none");
-  }
-
-  function showVal(e) {
-    let input = document.querySelector("input");
-    input.value = e.target.textContent
-  }
-
-  document.addEventListener("click", function(event) {
-    const clickedElement = event.target;
-    if (!inputWrap.contains(clickedElement)) {
-      inputWrap.classList.remove("border-bottom-n");
-      inputWrap.classList.remove("active");
-      option.classList.add("d-none");
-    }
-  });
-});
+let dropdown = document.querySelector(".dropdown")
+dropdown.onclick = function() {
+    dropdown.classList.toggle("active")
+}
